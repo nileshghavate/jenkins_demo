cb8b22ed2fb8a3f8f0b56c1eb1e47bde1f8fb7cc
Nilesh Ghavate <nsghavate@gmail.com>
2021-04-26 11:18:29 +0530

    Minor change in formatting

--- Demo.java
+++ Demo.java
@@ -2,10 +2,10 @@ class Demo
 {
     public static void main(String args[])
     {
-    System.out.println("Table");	
+    System.out.println("Table of 2");	
 	for(int i=0; i<10;i++)
 	{        
-		System.out.println("Value of 2 * "+i+" = "+(2*i));
+		System.out.println("Value of [2 * "+i+"] = "+(2*i));
 	}
     }
 }
