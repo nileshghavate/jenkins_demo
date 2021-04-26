0b7526d5d43a0f680e95bfc9327df2331b995f12
Nilesh Ghavate <nsghavate@gmail.com>
2021-04-26 11:07:58 +0530

    Initial commit

---
+++ Demo.java
@@ -0,0 +1,11 @@
+class Demo
+{
+    public static void main(String args[])
+    {
+    System.out.println("Table");	
+	for(int i=0; i<10;i++)
+	{        
+		System.out.println("Value of 2 * "+i+" = "+(2*i));
+	}
+    }
+}
